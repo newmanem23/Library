a03d85a9b95229bcfb70de2abd3354243f9441fd
Ed Newman <newmanem23@gmail.com>
2023-11-01 13:39:31 -0400

    Write add book function

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
+
 const myLibrary = [];
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 300, "Yes")
-myLibrary.push(theHobbit)
+// Page Elements
 const books = document.getElementById('books')
 const addBookBtn = document.querySelector('.add-book')
 const modal = document.querySelector('#book-dialog')
-const modalClose = document.querySelector('#modal-close')
-updateLibrary(myLibrary)
+const modalCloseBtn = document.querySelector('#modal-close')
+const bookForm = document.querySelector('#book-form')
 
+// Test code to add a book to the page
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 300, false)
+// myLibrary.push(theHobbit)
+// updateLibrary(myLibrary)
+
+// User Interface Listeners
 addBookBtn.addEventListener("click", () => {
     modal.showModal();
   });
 
-modalClose.addEventListener("click", () => {
+modalCloseBtn.addEventListener("click", () => {
     modal.close();
+    bookForm.reset()
 });
 
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formElements = bookForm.elements;
+    const title = formElements["title"].value;
+    const author = formElements["author"].value;
+    const numPages = formElements["numPages"].value;
+    const read = formElements["check-read"].checked;
+
+    const newBook = new Book(title, author, numPages, read);
+    console.log(newBook);
+    myLibrary.push(newBook)
+    console.log(bookForm.elements["check-read"].checked)
+    modal.close();
+    bookForm.reset();
+    updateLibrary(myLibrary);
+});
+
+
+// Functions
 function updateLibrary(library) {
+    books.innerHTML = '';
     for (let book of library) {
-        createBookCard(book)
+        createBookCard(book);
     }
 }
 
@@ -30,6 +57,7 @@ function Book(title, author, numPages, read) {
 }
 
 function createBookCard(book) {
+    // Create elements
     const bookCard = document.createElement('div')
     const title = document.createElement('p')
     const author = document.createElement('p')
@@ -37,20 +65,33 @@ function createBookCard(book) {
     const readBtn = document.createElement('button')
     const removeBtn = document.createElement('button')
 
-    bookCard.classList.add('book')
-    readBtn.classList.add('read')
-    removeBtn.classList.add('remove')
-
+    // Assign element contents and classes
     title.textContent = `"${book.title}"`
     author.textContent = `${book.author}`
     numPages.textContent = `${book.numPages} Pages`
-    readBtn.textContent = "Read"
     removeBtn.textContent = "Remove"
-
+    bookCard.classList.add('book')
+    removeBtn.classList.add('remove')
+    
+    if (book.read === true){
+        readBtn.textContent = "Read"
+        readBtn.classList.add('read')
+    }
+    else {
+        readBtn.textContent = "Not Read";
+        readBtn.classList.add('unread')
+    }
+    
+    // Assemble element and children
     bookCard.appendChild(title)
     bookCard.appendChild(author)
     bookCard.appendChild(numPages)
     bookCard.appendChild(readBtn)
     bookCard.appendChild(removeBtn)
     books.appendChild(bookCard)
+}
+
+function addBook(event) {
+
+
 }
